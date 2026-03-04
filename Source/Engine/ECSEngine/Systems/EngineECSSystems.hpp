7b37eb1d6461c129ab938e794b9217ed03a63184
EmilOlofsson <b.emil.olofsson@gmail.com>
2026-03-04 01:51:47 +0100

    Removed unused input view

---
+++ Source/Engine/ECSEngine/Systems/EngineECSSystems.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Engine/ECSEngine/Systems/RenderSystem.hpp"
+#include "Engine/ECSEngine/Systems/RenderLightSystem.hpp"
+#include "Engine/ECSEngine/Systems/CameraSystem.hpp"
+#include "Engine/ECSEngine/Systems/AnimationSystem.hpp"
+#include "Engine/ECSEngine/Systems/RotatingMovementSystem.hpp"
+#include "Engine/ECSEngine/Systems/NavmeshSystem.hpp"
+#include "Engine/ECSEngine/Systems/DebugShapeSystem.hpp"
